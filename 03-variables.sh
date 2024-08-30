c9409a714f9113fe5b28681631d428b2c503bcff
salmasadiq <salmasadiq.sk@gmail.com>
2024-08-30 19:54:48 +0530

    variables

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 PERSON1=Ramesh
-Person2=Suresh
+PERSON2=Suresh
 
 
-echo "Person1:: Hi ${Person2}, how are you ?"
-echo "${Person2}:: Hello Person1. I am fine. How are you doing?"
+
+echo "$PERSON1:: Hi ${PERSON2}, how are you ?"
+echo "${PERSON2}:: Hello $PERSON1. I am fine. How are you doing?"
+echo "$PERSON1:: I am doig good ${PERSON2}. What's goig on?"
+echo "${PERSON2}:: I started learning Shell Script $PERSON1"
