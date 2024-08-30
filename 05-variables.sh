031cffea82ad1a3541f22b73c14c2277f3ba0e55
salmasadiq <salmasadiq.sk@gmail.com>
2024-08-30 19:58:49 +0530

    variables

--- 05-variables.sh
+++ 05-variables.sh
@@ -5,3 +5,10 @@ echo "Please enter your username::"
 read USERNAME
 
 echo "Username entered is: $USERNAME"
+
+echo "please enter your password::"
+
+read -s PASSWORD
+
+echo "password entered is: $PASSWORD"
+
