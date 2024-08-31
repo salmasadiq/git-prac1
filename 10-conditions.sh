eae357150a07baae2f4d23171b7b72446629c6bf
salmasadiq <salmasadiq.sk@gmail.com>
2024-08-31 22:10:54 +0530

    variables

---
+++ 10-conditions.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+NUMBER=$1
+
+if [ $NUMBER -gt 20 ] #gt, lt, eq, ne
+then
+    echo "Given Number: $NUMBER is greater than 20"
+else
+    echo "Given Number: $UMER is less than 20"
+fi
+
